94bf7d53e8eb446e01dd9d4ccdadfc72c9beccb1
Thierry Berger <contact@thierryberger.com>
2022-07-18 16:28:14 +0200

    fixes to launch apps (windows + loop)

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
 
         let mut index: usize;
         loop {
-            if let Ok(i) = read_line("enter a game id:") {
+            if let Ok(i) = read_line("enter a game id: ") {
                 if i < paths.len() {
                     index = i;
                     break;
@@ -19,26 +19,26 @@ fn main() {
             }
         }
         let chosen_path = paths[index].clone();
-        start_app(chosen_path);
+        if let Ok(mut child) = start_app(chosen_path) {
+            child.wait();
+        }
     }
 }
 
 fn start_app(chosen_path: std::path::PathBuf) -> io::Result<std::process::Child> {
-    std::env::set_current_dir(chosen_path.clone());
-    let paths = std::fs::read_dir("./").unwrap();
-    let path_launch = std::fs::canonicalize("./").unwrap();
+    let paths = std::fs::read_dir(&chosen_path).unwrap();
+    let path_launch = std::fs::canonicalize(&chosen_path).unwrap();
     let paths: Vec<std::path::PathBuf> = paths.map(|p| p.unwrap().path()).collect();
     let asset_root = path_launch.clone();
     for (i, path) in paths.iter().enumerate() {
         println!("{}: Name: {}", i, path.display());
         // FIXME: this still fails to load assets in bevy because of "current_exe" stil referring to Launcher https://github.com/bevyengine/bevy/pull/1801
-        match Command::new(dbg!(path.display().to_string()))
+        match Command::new(path.display().to_string())
             .env_remove("CARGO_MANIFEST_DIR")
             // Workaround over https://github.com/bevyengine/bevy/issues/5345
-            .env("CARGO_MANIFEST_DIR", dbg!(asset_root.clone()))
-            
-            .env("BEVY_ASSET_ROOT", dbg!(asset_root.clone()))
-            .current_dir(dbg!(path_launch.clone()))
+            .env("CARGO_MANIFEST_DIR", &asset_root)
+            .env("BEVY_ASSET_ROOT", &asset_root)
+            .current_dir(&path_launch)
             .spawn()
         {
             Ok(child) => {
